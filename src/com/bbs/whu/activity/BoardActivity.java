f351e6af697a8005c454afe171e0861dc721238c
wangwang4git <wangwang4whu@126.com>
2013-01-13 19:45:50 +0800

    添加版块搜索功能。
    Signed-off-by: wangwang4git <wangwang4whu@126.com>

--- src/com/bbs/whu/activity/BoardActivity.java
+++ src/com/bbs/whu/activity/BoardActivity.java
@@ -4,11 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.bbs.whu.R;
 import com.bbs.whu.adapter.BoardAdapter;
@@ -28,6 +38,19 @@ public class BoardActivity extends Activity {
 	private List<List<Board>> childs = new ArrayList<List<Board>>();
 	// 接收请求数据的handler
 	Handler mHandler;
+	
+	// 搜索匹配数据源
+	private List<String> mSearchBoardsList = new ArrayList<String>();
+	// 搜索匹配适配器
+	private ArrayAdapter<String> mSearchAdapter;
+	// 搜索布局
+	private LinearLayout mSearchLinearLayout;
+	// 搜索输入框
+	private AutoCompleteTextView mSearchEditText;
+	// 搜索确定按钮
+	private ImageView mSearchConfirmButton;
+	// 触发搜索布局显示按钮
+	private Button mShowSearch;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -58,6 +81,38 @@ public class BoardActivity extends Activity {
 	private void initView() {
 		mExpandableListView = (ExpandableListView) findViewById(R.id.board_elist);
 		mExpandableListView.setGroupIndicator(null);
+		
+		// 获取AutoCompleteTextView对象，输入框
+		mSearchEditText = (AutoCompleteTextView) findViewById(R.id.board_search_autocompletetextview);
+		// 绑定搜索匹配数据源
+		mSearchAdapter = new ArrayAdapter<String>(this,
+				R.layout.board_search_match_item, mSearchBoardsList);
+		// 给AutoCompleteTextView对象指定ArrayAdapter对象
+		mSearchEditText.setAdapter(mSearchAdapter);
+		
+		// 获取确定按钮
+		mSearchConfirmButton = (ImageView) findViewById(R.id.board_search_confirm);
+		// 获取搜索布局
+		mSearchLinearLayout = (LinearLayout) findViewById(R.id.board_search_linearlayout);
+		// 获取触发搜索布局显示按钮
+		mShowSearch = (Button) findViewById(R.id.board_search_show_linearlayout_button);
+
+		mSearchConfirmButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				searchLayoutShow(false);
+				searchTheBoard(mSearchEditText.getText().toString());
+			}
+		});
+
+		mShowSearch.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				searchLayoutShow(true);
+			}
+		});
 	}
 
 	/**
@@ -133,5 +188,70 @@ public class BoardActivity extends Activity {
 		}
 		// 刷新ListView
 		mAdapter.notifyDataSetChanged();
+		
+		mSearchBoardsList.clear();
+		// 构造搜索匹配数据源
+		for (int i = 0; i < boardBeanList.size(); ++i) {
+			for (int j = 0; j < boardBeanList.get(i).getBoards().size(); ++j) {
+				mSearchBoardsList.add(boardBeanList.get(i).getBoards().get(j)
+						.getName().getAttributeValue());
+				mSearchBoardsList.add(boardBeanList.get(i).getBoards().get(j)
+						.getId().getAttributeValue());
+			}
+		}
+		mSearchAdapter.notifyDataSetChanged();
+	}
+	
+	/**
+	 * 搜索布局显示与隐藏切换
+	 * 
+	 * @param bool
+	 *            true，显示；false，隐藏
+	 */
+	void searchLayoutShow(boolean bool) {
+		if (true == bool) {
+			// 清空搜索输入框
+			mSearchEditText.setText("");
+			// 显示
+			mSearchLinearLayout.setVisibility(View.VISIBLE);
+			mShowSearch.setVisibility(View.INVISIBLE);
+		} else {
+			// 隐藏软键盘
+			InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(mSearchEditText.getWindowToken(), 0);
+			mSearchLinearLayout.setVisibility(View.INVISIBLE);
+			mShowSearch.setVisibility(View.VISIBLE);
+		}
+	}
+	
+	/**
+	 * 搜索输入的版块名，并进行界面跳转
+	 * 
+	 * @param keyword
+	 *            输入的版块名
+	 */
+	void searchTheBoard(String keyword) {
+		System.out.println(keyword);
+		// 遍历查找
+		for (int i = 0; i < childs.size(); ++i) {
+			for (int j = 0; j < childs.get(i).size(); ++j) {
+				// 查询该版块名存在
+				if (keyword.equals(childs.get(i).get(j).getName()
+						.getAttributeValue())
+						|| keyword.equals(childs.get(i).get(j).getId()
+								.getAttributeValue())) {
+					// 跳转到帖子列表界面
+					Intent mIntent = new Intent(this, TopicActivity.class);
+					// 添加参数 app=topics&board=PieFriends&page=1
+					mIntent.putExtra("board", childs.get(i).get(j).getId()
+							.getAttributeValue());
+					this.startActivity(mIntent);
+					return;
+				}
+			}
+		}
+		// 查询该版块名不存在
+		Toast.makeText(this, "版面不存在", Toast.LENGTH_SHORT).show();
+		return;
 	}
 }

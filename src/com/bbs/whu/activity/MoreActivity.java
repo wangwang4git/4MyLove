48fe56230f97b0ca741106ec606920801403e99c
double0291 <double0291@126.com>
2013-02-26 16:52:39 +0800

    添加“关于”界面

--- src/com/bbs/whu/activity/MoreActivity.java
+++ src/com/bbs/whu/activity/MoreActivity.java
@@ -1,6 +1,7 @@
 package com.bbs.whu.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -14,16 +15,12 @@ import com.bbs.whu.utils.MyApplication;
 import com.bbs.whu.utils.MyFileUtils;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-public class MoreActivity extends Activity {
+public class MoreActivity extends Activity implements OnClickListener {
 	private ViewGroup mCleanCache = null;
+	private ViewGroup mAbout = null;
 
 	// 图片异步下载下载器
 	private ImageLoader imageLoader = ImageLoader.getInstance();
-
-	// 清空内存缓存调用方法
-	// imageLoader.clearMemoryCache();
-	// 清空文件缓存调用方法
-	// imageLoader.clearDiscCache();
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -41,32 +38,42 @@ public class MoreActivity extends Activity {
 		return false;
 	}
 
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.more_list_clean_cache:
+			// 清空本地文件缓存
+			// 删除/whubbs/data/cache/username/文件夹
+			MyFileUtils.delFolder(MyFileUtils
+					.getSdcardDataCacheDir(((MyApplication) getApplication())
+							.getName()));
+			// 清空图片下载器内存缓存
+			imageLoader.clearMemoryCache();
+			// 清空图片下载器文件缓存
+			imageLoader.clearDiscCache();
+			Toast.makeText(
+					getApplicationContext(),
+					((TextView) ((ViewGroup) view)
+							.findViewById(R.id.more_list_clean_cache_textview))
+							.getText().toString(), Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.more_list_about:
+			// 跳转到“关于”页面
+			startActivity(new Intent(this, AboutActivity.class));
+			break;
+		}
+	}
+
 	/**
 	 * 初始化控件
 	 */
 	private void initView() {
-		// 
+		// 清除缓存
 		mCleanCache = (ViewGroup) findViewById(R.id.more_list_clean_cache);
-		mCleanCache.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				// 清空本地文件缓存
-				// 删除/whubbs/data/cache/username/文件夹
-				MyFileUtils.delFolder(MyFileUtils
-						.getSdcardDataCacheDir(((MyApplication) getApplication())
-								.getName()));
-				// 清空图片下载器内存缓存
-				imageLoader.clearMemoryCache();
-				// 清空图片下载器文件缓存
-				imageLoader.clearDiscCache();
-				Toast.makeText(
-						getApplicationContext(),
-						((TextView) ((ViewGroup) v)
-								.findViewById(R.id.more_list_clean_cache_textview))
-								.getText().toString(), Toast.LENGTH_SHORT)
-						.show();
-			}
-		});
+		mCleanCache.setOnClickListener(this);
+
+		// 关于
+		mAbout = (ViewGroup) findViewById(R.id.more_list_about);
+		mAbout.setOnClickListener(this);
 	}
 }

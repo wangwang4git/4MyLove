73b732272c5aeaf95ea4686405a3e7ef712498bc
double0291 <double0291@126.com>
2013-04-03 11:11:18 +0800

    在线好友列表

--- src/com/bbs/whu/adapter/OnlineFriendAdapter.java
+++ src/com/bbs/whu/adapter/OnlineFriendAdapter.java
@@ -3,13 +3,17 @@ package com.bbs.whu.adapter;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bbs.whu.R;
+import com.bbs.whu.activity.MailSendActivity;
+import com.bbs.whu.model.FriendBean;
 
 /**
  * 在线好友列表数据适配器
@@ -19,7 +23,7 @@ import com.bbs.whu.R;
  */
 public class OnlineFriendAdapter extends MyBaseAdapter {
 
-	public OnlineFriendAdapter(Context context, ArrayList<String> items,
+	public OnlineFriendAdapter(Context context, ArrayList<FriendBean> items,
 			int rLayoutList) {
 		super(context, items, rLayoutList);
 	}
@@ -32,22 +36,36 @@ public class OnlineFriendAdapter extends MyBaseAdapter {
 			convertView = LayoutInflater.from(context).inflate(
 					this.mRLayoutList, null);
 			// 获取列表元素中的控件对象
-			holder.holderOnlineFriendNumber = (TextView) convertView
-					.findViewById(R.id.online_frined_number);
 			holder.holderOnlineFriendHeadPortrait = (ImageView) convertView
-					.findViewById(R.id.online_frined_head_portrait);
+					.findViewById(R.id.online_friend_head);
 			holder.holderOnlineFriendName = (TextView) convertView
-					.findViewById(R.id.online_frined_name);
+					.findViewById(R.id.online_friend_name);
+			holder.holderOnlineFriendMail = (ImageView) convertView
+					.findViewById(R.id.online_friend_mail);
 			// 设置控件集到convertView
 			convertView.setTag(holder);
 		} else {
 			holder = (OnlineFriendViewHolder) convertView.getTag();
 		}
 		// 从数据源获取在线好友名称
-		String name = (String) mItems.get(position);
+		final FriendBean friend = (FriendBean) mItems.get(position);
 
 		// 填充控件
-		holder.holderOnlineFriendName.setText(name);
+		holder.holderOnlineFriendName.setText(friend.getID());
+		if (friend.isOnline()) {
+			holder.holderOnlineFriendName.setTextColor(context.getResources()
+					.getColor(R.color.online_friend_text_color));
+		}
+
+		holder.holderOnlineFriendMail.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 跳转到发信界面
+				Intent mIntent = new Intent(context, MailSendActivity.class);
+				mIntent.putExtra("sender", friend.getID());
+				context.startActivity(mIntent);
+			}
+		});
 
 		return convertView;
 	}

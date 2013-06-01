23a619739732998a8be506dc1c1117d5446f4d16
double0291 <double0291@126.com>
2013-06-01 09:35:43 +0800

    1、浏览帖子最多加载30页改为300页，刷死那帮家伙 2、帖子的评论显示时间

--- src/com/bbs/whu/utils/MyConstants.java
+++ src/com/bbs/whu/utils/MyConstants.java
@@ -40,7 +40,7 @@ public class MyConstants {
 	// 滑动退出的y轴最小像素间距
 	public static final int MAX_GAP_Y = 100;
 	// 版块列表加载的页数上限
-	public static final int TOPIC_MAX_PAGES = 30;
+	public static final int TOPIC_MAX_PAGES = 300;
 
 	// 客户端更新URL
 	public static final String UPDATE_CLIENT_URL = "http://bbs.whu.edu.cn/mobileUpdate/update.xml";
